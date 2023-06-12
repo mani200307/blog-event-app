43ab1fe8cf57ea955fdb3eae000430c4c3c4ef36
mani200307 <mani89509@gmail.com>
2023-06-12 16:35:53 +0530

    View blogs added

--- app/blog/view/page.jsx
+++ app/blog/view/page.jsx
@@ -1,18 +1,39 @@
+"use client"
+
 import Header from '@/app/components/Header'
-import HomeCard from '@/app/components/HomeCard'
-import React from 'react'
-import blogImg from '../../assets/blog.png'
+import React, { useEffect, useState } from 'react'
+import BlogCard from '@/app/components/BlogCard'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 const page = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+
+        const supabase = createClientComponentClient()
+
+        const getData = async () => {
+            const { data, error } = await supabase
+                .from('blogs')
+                .select("*")
+
+            console.log(data);
+            setBlogs(data);
+        }
+
+        getData();
+
+    }, [])
+
     return (
         <div className="h-auto bg-white pt-8 px-4 flex-1">
             <Header type='View Blogs' />
-            <HomeCard type='Blogs' linkUrl='blog' imgUrl={blogImg} desc="Make your own blog, View others blogs" />
-            <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
-            <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
-            <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
-            <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
-            <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
+            <div className='mt-10 grid lg:grid-cols-3 lg:gap-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-2'>
+                {blogs.map((blog, index) => (
+                    <BlogCard name={blog.title} key={index} authName={blog.authorName} desc={blog.body} categ={blog.category} />
+                ))}
+            </div>
         </div>
     )
 }
---
+++ app/components/BlogCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+const BlogCard = ({ name, authName, categ, desc }) => {
+    return (
+        <div className="rounded-md lg:w-72 md:w-96 sm:w-96 xs:w-72 w-full bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">
+                    <div>{name}</div>
+                    <div className="badge badge-secondary">{categ}</div>
+                </h2>
+                <p>{desc}</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">{authName}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogCard
