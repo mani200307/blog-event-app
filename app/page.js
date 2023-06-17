d5e37b601772603bf47139f37ed91ce39c95f812
mani200307 <mani89509@gmail.com>
2023-06-17 18:40:20 +0530

    Fixed homepage overflow

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="h-screen overflow-scroll pt-8 px-4 flex-1">
       <Header type="" />
-      <div className="flex -ml-7 flex-row justify-center md:flex-row xs:flex-col mt-5 gap-6">
+      <div className="flex flex-row justify-center md:flex-row xs:flex-col mt-5 gap-6">
         <HomeCard type='Blogs' linkUrl='blog' imgUrl={blogImg} desc="Make your own blog, View others blogs" />
         <HomeCard type='Events' linkUrl='event' imgUrl={blogImg} desc="Schedule your own event, Browse several events" />
       </div>
