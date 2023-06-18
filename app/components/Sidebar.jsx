04f239152551f64770e5bb218ec2b10828d2c0ca
mani200307 <mani89509@gmail.com>
2023-06-18 17:51:08 +0530

    Active page link

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
 import Link from 'next/link';
 import { Bars3Icon } from '@heroicons/react/24/outline';
+import { create } from 'zustand';
+
+const useActivePageStore = create((set) => ({
+    activePage: '',
+    setActivePage: (page) => set(() => ({ activePage: page })),
+}));
 
 const Sidebar = () => {
+    const setActivePage = useActivePageStore((state) => state.setActivePage);
+    const activePage = useActivePageStore((state) => state.activePage);
 
     return (
         <div className="drawer bg-white gap-5 flex w-80 flex-col md:w-1/12 sm:w-1/12 xs:w-1/6 lg:mr-32 md:mr-3 xs:mr-2">
@@ -18,19 +28,68 @@ const Sidebar = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <div className="menu p-4 w-60 xs:w-44 h-full bg-black text-white">
+                    <div className="menu flex justify-between p-4 w-60 xs:w-44 h-full bg-black text-white">
                         <div className='flex items-center justify-between'>
                             <div className='flex items-center pl-1 gap-4'>
-                                <Link href='/'><h1 className='text-2xl font-medium cursor-pointer'>BlogEvent</h1></Link>
+                                <Link href="/">
+                                    <span
+                                        className={`className='font-medium cursor-pointer' ${activePage === 'createBlog' ? 'bg-gray-800' : ''
+                                            }`}
+                                        onClick={() => setActivePage('')}
+                                    >
+                                        <h1 className='text-2xl'>BlogEvent</h1>
+                                    </span>
+                                </Link>
                             </div>
                         </div>
-                        <div className='divider'></div>
                         <div className='flex flex-col items-start gap-2 mt-3'>
-                            <Link href='/blog/create' className='text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap'><h1>Create Blog</h1></Link>
-                            <Link href='/blog/view' className='text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap'><h1>View Blogs</h1></Link>
-                            <Link href='/event/create' className='text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap'><h1>Schedule Event</h1></Link>
-                            <Link href='/event/view' className='text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap'><h1>View Events</h1></Link>
+                            <Link href="/blog/create">
+                                <span
+                                    className={`text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap ${activePage === 'createBlog' ? 'bg-gray-800' : ''
+                                        }`}
+                                    onClick={() => setActivePage('createBlog')}
+                                >
+                                    <h1>Create Blog</h1>
+                                </span>
+                            </Link>
+                            <Link href="/blog/view">
+                                <span
+                                    className={`text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap ${activePage === 'viewBlogs' ? 'bg-gray-800' : ''
+                                        }`}
+                                    onClick={() => setActivePage('viewBlogs')}
+                                >
+                                    <h1>View Blogs</h1>
+                                </span>
+                            </Link>
+                            <Link href="/event/create">
+                                <span
+                                    className={`text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap ${activePage === 'createEvent' ? 'bg-gray-800' : ''
+                                        }`}
+                                    onClick={() => setActivePage('createEvent')}
+                                >
+                                    <h1>Schedule Event</h1>
+                                </span>
+                            </Link>
+                            <Link href="/event/view">
+                                <span
+                                    className={`text-xl flex items-center cursor-pointer hover:bg-gray-800 p-2 rounded w-full overflow-hidden whitespace-nowrap ${activePage === 'viewEvents' ? 'bg-gray-800' : ''
+                                        }`}
+                                    onClick={() => setActivePage('viewEvents')}
+                                >
+                                    <h1>View Events</h1>
+                                </span>
+                            </Link>
                         </div>
+                        <div className='divider'></div>
+                        <Link href='/logout'>
+                            <span
+                                className={`text-xl flex items-center cursor-pointer hover:bg-gray-900 p-2 rounded w-full overflow-hidden whitespace-nowrap ${activePage === 'viewEvents' ? 'bg-gray-800' : ''
+                                    }`}
+                                onClick={() => setActivePage('')}
+                            >
+                                <h1>Logout</h1>
+                            </span>
+                        </Link>
                     </div>
                 </div>
             </div>
